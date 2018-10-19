54a71b7bc3b5e01676715ebb2d976a38043b3f61
Vincent Beffara <vbeffara@gmail.com>
2018-10-20 00:01:05 +0200

    FLTK from git, to get the Mojave-compatible version

--- fltk.py
+++ fltk.py
@@ -3,15 +3,19 @@ from conans import ConanFile, CMake, tools
 
 class FltkConan(ConanFile):
     name = "fltk"
-    version = "1.3.4"
+    version = "1.4.0+dd8e60a95"
     license = "LGPL"
     url = "http://www.fltk.org/"
     description = "FLTK is a cross-platform C++ GUI toolkit"
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
+    requires = "libjpeg/9c@bincrafters/stable"
 
     def source(self):
-        tools.get("http://fltk.org/pub/fltk/1.3.4/fltk-1.3.4-2-source.tar.gz")
+        self.run("git clone http://github.com/fltk/fltk.git")
+        self.run("git -C fltk checkout dd8e60a95")
+        tools.replace_in_file("fltk/CMakeLists.txt", "project(FLTK)",
+                              "project(FLTK) \ninclude(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake) \nconan_basic_setup(KEEP_RPATHS)")
 
     def build(self):
         cmake = CMake(self)
@@ -19,11 +23,11 @@ class FltkConan(ConanFile):
         if self.settings.os == "Linux":
             cmake.definitions["CMAKE_C_FLAGS"] = "-fPIC"
             cmake.definitions["CMAKE_CXX_FLAGS"] = "-fPIC"
-        cmake.configure(source_folder="fltk-1.3.4-2", )
+        cmake.configure(source_folder="fltk", )
         cmake.build()
 
     def package(self):
-        self.copy("FL/*", dst="include", src="fltk-1.3.4-2")
+        self.copy("FL/*", dst="include", src="fltk")
         self.copy("*abi-version.h", dst="include/FL", keep_path=False)
         self.copy("*fltk.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
